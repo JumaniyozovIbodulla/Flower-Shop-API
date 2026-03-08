f92e37a0313f32d7f2a17f0c6f38b44c301235c8
JumaniyozovIbodulla <jumaniyozovibodulla07@gmail.com>
2026-03-08 15:55:44 +0500

    done architect

--- migrations/000001_create_tables.down.sql
+++ migrations/000001_create_tables.down.sql
@@ -1,10 +1,19 @@
 -- TABLES
+DROP TABLE IF EXISTS "order_items";
+DROP TABLE IF EXISTS "orders";
+DROP TABLE IF EXISTS "cart_items";
+DROP TABLE IF EXISTS "carts";
+DROP TABLE IF EXISTS "flower_media";
+DROP TABLE IF EXISTS "flowers";
 DROP TABLE IF EXISTS "user_roles";
 DROP TABLE IF EXISTS "role_permissions";
 DROP TABLE IF EXISTS "permissions";
 DROP TABLE IF EXISTS "roles";
 DROP TABLE IF EXISTS "users";
+-- SEQUENCE
+DROP SEQUENCE IF EXISTS "order_number_seq";
 -- TYPES
-DROP TYPE IF EXISTS "statuses";
+DROP TYPE IF EXISTS "media_type";
+DROP TYPE IF EXISTS "order_status";
 DROP TYPE IF EXISTS "languages";
 
--- migrations/000001_create_tables.up.sql
+++ migrations/000001_create_tables.up.sql
@@ -1,5 +1,9 @@
 CREATE TYPE "languages" AS ENUM('uz','ru','en');
-CREATE TYPE "statuses" AS ENUM('pending','confirmed', 'delivered', 'cancelled');
+CREATE TYPE "order_status" AS ENUM('pending','confirmed', 'delivered', 'cancelled');
+CREATE TYPE "media_type" AS ENUM ('image', 'video');
+
+CREATE SEQUENCE "order_number_seq" START 10000;
+
 
 CREATE TABLE IF NOT EXISTS "users" (
     "id"                UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
@@ -12,14 +16,14 @@ CREATE TABLE IF NOT EXISTS "users" (
 
 CREATE TABLE IF NOT EXISTS "roles" (
     "id"            UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
-    "name"          VARCHAR(255) NOT NULL, -- admin, manager
+    "name"          VARCHAR(255) NOT NULL UNIQUE, -- admin, manager
     "description"   TEXT DEFAULT '',
     "created_at"    TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS "permissions" (
     "id"            UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
-    "name"          VARCHAR(255) NOT NULL, -- create_flower, delete_flower
+    "name"          VARCHAR(255) NOT NULL UNIQUE, -- create_flower, delete_flower
     "description"   TEXT,
     "created_at"    TIMESTAMPTZ DEFAULT NOW()
 );
@@ -34,5 +38,58 @@ CREATE TABLE IF NOT EXISTS "user_roles" (
     "user_id"   UUID NOT NULL REFERENCES "users"("id") ON DELETE CASCADE,
     "role_id"   UUID NOT NULL REFERENCES "roles"("id") ON DELETE CASCADE,
     PRIMARY KEY ("user_id", "role_id")
+);
+
+
+CREATE TABLE IF NOT EXISTS "flowers" (
+    "id"            UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    "title"         VARCHAR(255) NOT NULL,
+    "description"   TEXT DEFAULT '',
+    "price"         BIGINT NOT NULL CHECK ("price" >= 0),
+    "stock"         INT NOT NULL CHECK ("stock" >= 0) DEFAULT 0,
+    "is_active"     BOOLEAN NOT NULL DEFAULT TRUE,
+    "created_at"    TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS "flower_media" (
+    "id"                    UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    "flower_id"             UUID NOT NULL REFERENCES "flowers"("id") ON DELETE CASCADE,
+    "content_type"          media_type NOT NULL,
+    "object_name"           TEXT NOT NULL,
+    "created_at"            TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS "carts" (
+    "id"            UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    "customer_id"   UUID NOT NULL REFERENCES "users"("id"),
+    "created_at"    TIMESTAMPTZ DEFAULT NOW()
+);
+
+
+CREATE TABLE IF NOT EXISTS "cart_items" (
+    "id"        UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    "cart_id"   UUID NOT NULL REFERENCES "carts"("id") ON DELETE CASCADE,
+    "flower_id" UUID NOT NULL REFERENCES "flowers"("id"),
+    "quantity"  INT NOT NULL CHECK ("quantity" > 0),
+    "price"     BIGINT NOT NULL CHECK("price" > 0)
+);
+
+CREATE TABLE IF NOT EXISTS "orders" (
+    "id"            UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    "order_number"  BIGINT UNIQUE DEFAULT (nextval('order_number_seq') * 7919),
+    "customer_id"   UUID NOT NULL REFERENCES "users"("id"),
+    "total_price"   BIGINT NOT NULL CHECK("total_price" > 0),
+    "status"        order_status NOT NULL DEFAULT 'pending',
+    "created_at"    TIMESTAMPTZ DEFAULT NOW(),
+    "updated_at"    TIMESTAMPTZ
+);
+
+
+CREATE TABLE IF NOT EXISTS "order_items" (
+    "id"        UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    "order_id"  UUID NOT NULL REFERENCES "orders"("id") ON DELETE CASCADE,
+    "flower_id" UUID NOT NULL REFERENCES "flowers"("id"),
+    "quantity"  INT NOT NULL CHECK ("quantity" > 0),
+    "price"     BIGINT NOT NULL CHECK("price" > 0)
 );
 
